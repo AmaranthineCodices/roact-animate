b8c82040df3244b321afc70a8f82252929750fa2
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-06-09 18:30:24 -0400

    don't publicly export Animation, AnimationSequence, and PrepareStep

--- src/init.lua
+++ src/init.lua
@@ -7,33 +7,34 @@ for _, class in ipairs({ "Frame", "ImageLabel", "ImageButton", "TextButton", "Te
 	RoactAnimate[class] = makeAnimatedComponent(class)
 end
 
+local Animation = require(script.Animation)
+local AnimationSequence = require(script.AnimationSequence)
+local PrepareStep = require(script.PrepareStep)
+
 RoactAnimate.Value = require(script.AnimatedValue)
 RoactAnimate.makeAnimatedComponent = makeAnimatedComponent
-RoactAnimate.Animation = require(script.Animation)
-RoactAnimate.AnimationSequence = require(script.AnimationSequence)
-RoactAnimate.PrepareStep = require(script.PrepareStep)
 
 -- Creates an animation for a value.
 function RoactAnimate.Animate(value, tweenInfo, to)
-	return RoactAnimate.Animation.new(value, tweenInfo, to)
+	return Animation.new(value, tweenInfo, to)
 end
 
 -- Creates an animation from a table of animations.
 -- These animations will be run one-by-one.
 function RoactAnimate.Sequence(animations)
-	return RoactAnimate.AnimationSequence.new(animations, false)
+	return AnimationSequence.new(animations, false)
 end
 
 -- Creates an animation from a table of animations.
 -- These animations will be run all at once.
 function RoactAnimate.Parallel(animations)
-	return RoactAnimate.AnimationSequence.new(animations, true)
+	return AnimationSequence.new(animations, true)
 end
 
 -- Creates a preparation step.
 -- This allows instantaneous resetting of a value prior to animations completing.
 function RoactAnimate.Prepare(value, to)
-	return RoactAnimate.PrepareStep.new(value, to)
+	return PrepareStep.new(value, to)
 end
 
 setmetatable(RoactAnimate, { __call = function(_, ...) return RoactAnimate.Animate(...) end; })
